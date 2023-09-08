8c6a2ab068344362eb934e660886f259380ef1fd
td6904 <td665pro@pm.me>
2023-09-08 15:56:07 +0200

    Prisma

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Post, Body } from "@nestjs/common";
+import { Request } from "express";
+import { AuthService } from "./auth.service";
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('signup')
+    signup(@Body() dto: any) {  
+        console.log({
+            dto,
+        }); 
+        return this.authService.signup();
+      }
+
+    @Post('signin')
+    signin() {
+        return this.authService.signin();
+    }
+}
+
